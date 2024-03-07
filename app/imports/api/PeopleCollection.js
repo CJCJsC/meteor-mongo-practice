4a1464ca57f2a61b8142744a3e888580974c1050
XxLummoxxX <46939185+XxLummoxxX@users.noreply.github.com>
2024-03-06 17:16:02 -1000

    task 4

--- app/imports/api/PeopleCollection.js
+++ app/imports/api/PeopleCollection.js
@@ -2,7 +2,7 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 
 const peopleSchema = new SimpleSchema(
-  {
+  { 
     first: String,
     last: String,
     age: SimpleSchema.Integer,
